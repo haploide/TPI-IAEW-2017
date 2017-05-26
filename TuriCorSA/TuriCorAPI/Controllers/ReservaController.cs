bf2fbce487d3b04ec80cfe09c1f3b4083bbd99d8
pablo657 <pablo657@hotmail.com>
2017-05-26 20:50:34 -0300

    creado controller y agregado cors

---
+++ TuriCorSA/TuriCorAPI/Controllers/ReservaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using TuriCorAPI.Models;
+
+namespace TuriCorAPI.Controllers
+{
+    [EnableCors(origins: "http://localhost:2253", headers: "*", methods: "*")]
+    public class ReservaController : ApiController
+    {
+    }
+}
--- TuriCorSA/TuriCorAPI/Models/ModelTuriCorSA.Designer.cs
+++ TuriCorSA/TuriCorAPI/Models/ModelTuriCorSA.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'I:\UTN\IAEW\Practicos\TPI\TPI-IAEW-2017\TuriCorSA\TuriCorAPI\Models\ModelTuriCorSA.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\pablo\Desktop\UTN 2017\IAEW\INTEGRADOR\TPI-IAEW-2017\TuriCorSA\TuriCorAPI\Models\ModelTuriCorSA.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
