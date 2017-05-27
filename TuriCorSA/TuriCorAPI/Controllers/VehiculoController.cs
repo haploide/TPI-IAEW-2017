a68779a1aed17327a9ecded444c9e7f77b9ca9fe
haploide <haploide1998@hotmail.com>
2017-05-26 21:46:34 -0300

    agregado get a vehiculos

--- TuriCorSA/TuriCorAPI/Controllers/VehiculoController.cs
+++ TuriCorSA/TuriCorAPI/Controllers/VehiculoController.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
-using TuriCorAPI.Models;
+using System.Threading.Tasks;
+
 namespace TuriCorAPI.Controllers
 {
     [EnableCors(origins: "http://localhost:2253", headers: "*", methods: "*")]
     public class VehiculoController : ApiController
     {
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var cliente = new ServiceReferenceReservaVehiculos.WCFReservaVehiculosClient();
+
+                var vehiculos = cliente.ConsultarVehiculosDisponibles(new ServiceReferenceReservaVehiculos.ConsultarVehiculosRequest()
+                {
+                    IdCiudad = 2,
+                    FechaHoraRetiro = new DateTime(2017, 1, 1),
+                    FechaHoraDevolucion= new DateTime(2017,3,27)
+                });
+
+                if (vehiculos == null)
+                {
+                    return NotFound();
+                }
+                return Ok(vehiculos);
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+
+
     }
 }
