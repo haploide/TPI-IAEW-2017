f1810f9ffef1c9c61dc935588bbb584b0eba9758
pablo657 <pablo657@hotmail.com>
2017-05-31 18:01:34 -0300

    controller ciudad "listo y probado por posmant"

---
+++ TuriCorSA/TuriCorAPI/Controllers/CiudadController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TuriCorAPI.Controllers
+{
+    [EnableCors(origins: "http://localhost:2253", headers: "*", methods: "*")]
+    public class CiudadController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var cliente = new ServiceReferenceReservaVehiculos.WCFReservaVehiculosClient();
+
+                var ciudades= cliente.ConsultarCiudades(new ServiceReferenceReservaVehiculos.ConsultarCiudadesRequest()
+                {
+                    IdPais = 1,
+                   
+                });
+
+                if (ciudades == null)
+                {
+                    return NotFound();
+                }
+                return Ok(ciudades);
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
